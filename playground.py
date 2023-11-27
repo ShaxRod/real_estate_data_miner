0edc367a4584b28c0d385787294cf8e2a18500f2
Shaqir <shaxrodgers@gmail.com>
2023-11-27 09:48:32 +0200

    working scraper for privateproperty.com

--- playground.py
+++ playground.py
@@ -18,15 +18,6 @@ data = {'title': 'div',
 dict = obj.scraper_dict(data)
 # find a way to get the listing unique id from json string somehow?
 
-
-# How do we handle multiple pages? We do a while loop
-
-province_url = f'https://www.privateproperty.co.za/for-sale/gauteng/3'
-area_container = {'div': 'contentHolder'}
-
-obj = webscrape(province_url, area_container)
-items = obj.scraper_items()
-
 from utils.websites import privateproperty
 
 areas = privateproperty().get_area_links()
