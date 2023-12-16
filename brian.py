d8ddbf2900c801017eb08d1cbd297f1162a1c213
Shaqir Rodgers <shaqir.rodgers@lulalend.co.za>
2023-12-16 20:25:24 +0200

    gets features for all individual properties

--- brian.py
+++ brian.py
@@ -9,26 +9,37 @@ def get_main_features(mit_data: pd.DataFrame):
     catch = dict()
     for i, link in enumerate(mit_data['url']):
         try:
-            dimensions = webscrape(url=link, container={'div': 'mainFeatures'}).scraper_items(False)[0].find_all('span')
+            dimensions = webscrape(url=link, container={'div': 'mainFeatures'}).scraper_items(False)
             print('dimensions: ', len(dimensions))
-            temp = dict()
-            temp['url'] = link
-            for dimension in dimensions:
-                dim_list = dimension.text.rsplit()
-                if len(dim_list) > 0:
-                    if dim_list[0] == 'Land' or dim_list[0] == 'Floor':
-                        temp[f'{dim_list[0]}_{dim_list[1]}'] = dim_list[2]
-                    elif dim_list[0] == 'Rates' or dim_list[0] == 'Levy':
-                        temp[f'{dim_list[0]}'] = dim_list[2]
+            if len(dimensions) != 0:
+                temp = dict()
+                temp['url'] = link
+                for dimension in dimensions[0].find_all('span'):
+                    dim_list = dimension.text.rsplit()
+                    if len(dim_list) > 0:
+                        if dim_list[0] == 'Land' or dim_list[0] == 'Floor':
+                            temp[f'{dim_list[0]}_{dim_list[1]}'] = dim_list[2]
+                        elif dim_list[0] == 'Rates' or dim_list[0] == 'Levy':
+                            temp[f'{dim_list[0]}'] = dim_list[2]
+
+                for key in ['url', 'Floor_Area', 'Land_Area', 'Rates', 'Levy']:
+                    if key not in temp.keys():
+                        temp[key] = '0'
+            else:
+                temp = {'url': link,
+                        'Floor_Area': '0',
+                        'Land_Area': '0',
+                        'Rates': '0',
+                        'Levy': '0'}
             print(temp)
         except:
-            print(i, 'fucked out')
+            print(i, 'tapped out')
             catch[i] = {'index': i,
                         'url': link}
             continue
         main_features[i] = temp
 
-    return main_features
+    return main_features, catch
 
 
 def mitdata():
@@ -42,5 +53,11 @@ def mitdata():
     scrape_frame['url'] = website + scrape_frame['href'].astype('str')
 
     woodstock = scrape_frame[scrape_frame['suburb'] == 'Woodstock'].copy(deep=True)
-
-    return
+    features, catch = get_main_features(woodstock)
+    feature_frame = pd.DataFrame(features)
+    feature_frame = feature_frame.transpose()
+    final_frame = pd.merge(left=woodstock,
+                           right=feature_frame,
+                           on='url',
+                           how='left')
+    return final_frame
--- utils/general.py
+++ utils/general.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 class data_structure_format:
 
-    def nest_dict_to_dataframe(self, nest_dict : dict):
+    def nest_dict_to_dataframe(self, nest_dict: dict):
 
         df = pd.DataFrame(nest_dict)
         df = df.swapaxes('index', 'columns')
